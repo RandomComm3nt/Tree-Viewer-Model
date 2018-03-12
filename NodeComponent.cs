2fdb4b4dbbbd3c2935e6202195a5703e1995cd64
Dan Armour <randomcomm3nt@hotmail.co.uk>
2018-03-12 22:14:38 +0000

    #137 - Component serialization

--- NodeComponent.cs
+++ NodeComponent.cs
@@ -10,15 +10,85 @@ namespace Assets.Scripts.Model.Data.TreeViewer
 {
 	public abstract class NodeComponent
 	{
+		private static Dictionary<string, Type> componentMap;
+		private static Dictionary<Type, List<FieldInfo>> componentFields;
+
+		public static Dictionary<string, Type> ComponentMap
+		{
+			get
+			{
+				if (componentMap == null)
+				{
+					componentMap = AppDomain.CurrentDomain.GetAssemblies().SelectMany(a => a.GetTypes()).ToDictionary(t => t.Name, t => t);
+				}
+				return componentMap;
+			}
+
+			set
+			{
+				componentMap = value;
+			}
+		}
+
+		public static Dictionary<Type, List<FieldInfo>> ComponentFields
+		{
+			get
+			{
+				if (componentFields == null)
+				{
+					componentFields = ComponentMap
+						.Values
+						.ToDictionary(t => t, t => t.GetFields()
+							.Where(f => f.GetCustomAttribute(typeof(ComponentPropertyAttribute)) != null)
+							.ToList());
+				}
+				return componentFields;
+			}
+
+			set
+			{
+				componentFields = value;
+			}
+		}
+
 		public XElement ToXml()
 		{
-			throw new NotImplementedException();
+			XElement node = new XElement("Component");
+			node.Add(new XElement("ComponentName", GetType().ToString()));
+			if (ComponentFields.ContainsKey(GetType()))
+			{
+				foreach (FieldInfo fi in ComponentFields[GetType()])
+				{
+					// write component 
+				}
+			}
+			return node;
 		}
 
 		public static NodeComponent FromXml(XElement component)
 		{
-			//Assembly.
-			throw new NotImplementedException();
+			Type T;
+			if (ComponentMap.TryGetValue(component.Element("ComponentName").Value, out T))
+			{
+				Object o = Activator.CreateInstance(T);
+				if (!(o is NodeComponent))
+				{
+					throw new Exception("Attempt to deserialize XML failed. Object of type " + o.GetType().Name + " could not be cast to NodeComponent");
+				}
+				if (ComponentFields.ContainsKey(T))
+				{
+					foreach (FieldInfo fi in ComponentFields[T])
+					{
+						// read component
+					}
+				}
+
+				return o as NodeComponent;
+			}
+			else
+			{
+				throw new Exception("Attempt to deserialize XML failed. Component of type " + component.Element("ComponentName").Value + " could not be found in component map");
+			}
 		}
 	}
 }
